36b29759d45efc29e4b55600bafffef0659c0889
fero-ons <fhajnovic.ons@gmail.com>
2017-07-20 18:48:44 +0100

    put all in classes

--- dashboards/searchsim/main.py
+++ dashboards/searchsim/main.py
@@ -5,10 +5,9 @@ import collections
 
 from bokeh.io import curdoc, show
 from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource, HoverTool, Div, Range1d, CustomJS
+from bokeh.models import ColumnDataSource, HoverTool, Div, Range1d, Text
 from bokeh.models.widgets import TextInput, DataTable, TableColumn, HTMLTemplateFormatter
 from bokeh.layouts import widgetbox, layout
-from bokeh.events import MouseMove, MouseEnter
 
 from os.path import dirname, join
 
@@ -18,7 +17,7 @@ from common import *
 
 # --- constants -----------------------------------------------------------
 
-MAX_SEARCH_RES = 50
+MAX_SEARCH_RES = 200
 MAX_BARS = 15
 ANALYSED_COLS = ['text', 'type', 'close_seg_text', 'all_inclusions', 'all_exclusions']
 DISPLAYED_COLS = ['survey_id', 'form_type', 'tr_code', 'text']
@@ -35,11 +34,20 @@ COL_WIDTHS['type'] = 200
 
 # --- data -----------------------------------------------------------
 
-class Data:
+class App:
+    qtext = None
+    nresults = None
+    res_table = None
+    bar_chart = None
+
+    search_res_df = None
+    base_df = None
+
+    results_src = None
+    similarity_src = None
+
     @classmethod
     def compute_sims_df(cls, selected_index):
-        """Brings back a dataframe with (among others) similarity scores against selected question"""
-
         df = cls.search_res_df.copy()
         df['text'] = df['text'].fillna('')
 
@@ -57,126 +65,141 @@ class Data:
         return df.iloc[:MAX_BARS]
 
     @classmethod
-    def update_search_res_source(cls, search_kw):
-        """updates the search results data source, i.e. the data of the table, based on the searched key word"""
+    def search_for_kw(cls, search_kw):
+        cls.update_results_src(search_kw)
+        cls.nresults.text = '{} results'.format(len(cls.search_res_df))
 
+    @classmethod
+    def update_results_src(cls, search_kw):
         cls.search_res_df = cls.base_df[cls.base_df['text'].str.contains(search_kw, na=False)]
         if len(cls.search_res_df) > MAX_SEARCH_RES:
             cls.search_res_df = cls.search_res_df.sample(MAX_SEARCH_RES)
 
-        if not hasattr(cls, 'search_res_source'):
-            cls.search_res_source = ColumnDataSource(cls.search_res_df)
+        if cls.results_src == None:  # creating
+            cls.results_src = ColumnDataSource(cls.search_res_df)
+            cls.results_src.on_change('selected', cls.selected_search_result_handler)
         else:
-            cls.search_res_source.data = ColumnDataSource(cls.search_res_df).data
+            cls.results_src.data = ColumnDataSource(cls.search_res_df).data
 
     @classmethod
-    def update_sims_source(cls, selected_index):
-        """updates the similarity data source (i.e. underlying data used by the bar chart), based on the selected index"""
-
+    def update_similarity_src(cls, selected_index):
         cls.sims_df = cls.compute_sims_df(selected_index)
         
-        if not hasattr(cls, 'sims_source'):
-            cls.sims_source = ColumnDataSource(cls.sims_df)
+        if cls.similarity_src == None:  # creating
+            cls.similarity_src = ColumnDataSource(cls.sims_df)
         else:
-            cls.sims_source.data = ColumnDataSource(cls.sims_df).data
+            cls.similarity_src.data = ColumnDataSource(cls.sims_df).data
+
+    @classmethod
+    def create_table(cls):
+        template = """<div class="tooltip-parent"><div class="tooltipped"><%= value %></div><div class="tooltip-text"><%= value %></div></div>"""
+
+        columns = [TableColumn(
+            field=c,
+            title=c,
+            width=COL_WIDTHS[c],
+            formatter=HTMLTemplateFormatter(template=template)
+        ) for c in DISPLAYED_COLS]
+
+        cls.res_table = DataTable(
+            source=cls.results_src,
+            columns=columns,
+            width=PAGE_WIDTH,
+            height=320,
+            editable=True
+        )
+        
+    @classmethod
+    def create_bar_chart(cls):
+        tooltip_fields = [
+            ("Survey", "@survey_id (@survey_name)"),
+            ("Form Type/Tr. code", "@form_type / @tr_code"),
+            ("Text", "@text")
+        ]
+
+        for col in ANALYSED_COLS:
+            bar_char_title = col.replace('_', ' ').title()
+            tooltip_fields.append((bar_char_title, '@{}'.format(col)))
+
+        hover = HoverTool(tooltips=tooltip_fields)
+
+        cls.bar_chart = figure(
+            plot_height=600,
+            plot_width=PAGE_WIDTH,
+            toolbar_location=None,
+            tools=[hover],
+            title="Select question",
+            y_range=Range1d(0, 1)
+        )
+        cls.bar_chart.vbar(
+            x="index",
+            top="similarity",
+            bottom=0,
+            width=0.5,
+            fill_color="color",
+            source=cls.similarity_src
+        )
+        cls.bar_chart.min_border_bottom = 200
+        cls.bar_chart.yaxis.axis_label = 'similarity'
+        cls.bar_chart.xaxis.axis_label = 'question'
+
+    @classmethod
+    def selected_search_result_handler(cls, attr, old, new):
+        cls.select_index(new['1d']['indices'][0])
+
+    @classmethod
+    def select_index(cls, selected_index):
+        cls.update_similarity_src(selected_index)
+
+        tr_code = cls.search_res_df.iloc[selected_index].name
+        cls.bar_chart.title.text = 'Top {} similar questions for question {}'.format(MAX_BARS, tr_code)
+
+    @classmethod
+    def create_qtext_box(cls):
+        cls.qtext = TextInput(title="Search question text")
+        cls.qtext.on_change('value', lambda attr, old, new: cls.search_for_kw(cls.qtext.value))
+
+    @classmethod
+    def create_nresults_div(cls):
+        cls.nresults = Div(text=str(MAX_SEARCH_RES))
+
+    @classmethod
+    def show(cls):
+        sizing_mode = 'fixed'
+
+        desc = Div(text=open(join(dirname(__file__), "description.html")).read(), width=PAGE_WIDTH)
+
+        l = layout([
+            [desc],
+            [cls.qtext, cls.nresults],
+            [cls.res_table],
+            [cls.bar_chart]
+        ], sizing_mode=sizing_mode)
+
+        curdoc().add_root(l)
+        curdoc().title = "Question bank exploration dashboard"
+
+        show(l)
 
     @classmethod
     def init(cls):
-        cls.selected_index = 0
+        INIT_INDEX = 0
+        INIT_KW = ''
 
         cls.base_df = load_clean_df()
 
-        cls.update_search_res_source('')
-        cls.update_sims_source(cls.selected_index)
+        cls.update_results_src(INIT_KW)
+        cls.update_similarity_src(INIT_INDEX)
 
-Data.init()
+        cls.create_table()
+        cls.create_bar_chart()
+        cls.create_qtext_box()
+        cls.create_nresults_div()
+
+        cls.select_index(INIT_INDEX)
+        cls.search_for_kw(INIT_KW)
+
+        cls.show()
 
 
-# --- table -----------------------------------------------------------
-
-temp = """<div class="tooltip-parent"><div class="tooltipped"><%= value %></div><div class="tooltip-text"><%= value %></div></div>"""
-temp = """<span href="#" data-toggle="tooltip" title="<%= value %>"><%= value %></span>"""
-
-columns = [TableColumn(
-    field=c,
-    title=c,
-    width=COL_WIDTHS[c],
-    formatter=HTMLTemplateFormatter(template=temp)
-) for c in DISPLAYED_COLS]
-
-search_res_table = DataTable(
-    source=Data.search_res_source,
-    columns=columns,
-    width=PAGE_WIDTH,
-    height=280,
-    editable=True
-)
-
-# --- bar chart -----------------------------------------------------------
-
-tooltip_fields = [
-    ("Survey ID", "@survey_id"),
-    ("Form Type", "@form_type"),
-    ("Tracking code", "@tr_code"),
-    ("Text", "@text")
-]
-
-for col in ANALYSED_COLS:
-    title = col.replace('_', ' ').title()
-    tooltip_fields.append((title, '@{}'.format(col)))
-
-hover = HoverTool(tooltips=tooltip_fields)
-
-sim_bar_chart = figure(
-    plot_height=600,
-    plot_width=PAGE_WIDTH,
-    title="",
-    toolbar_location=None,
-    tools=[hover],
-    y_range=Range1d(0, 1)
-)
-sim_bar_chart.vbar(
-    x="index",
-    top="similarity",
-    bottom=0,
-    width=0.5,
-    fill_color="color",
-    source=Data.sims_source
-)
-sim_bar_chart.min_border_bottom = 200
-sim_bar_chart.yaxis.axis_label = 'similarity'
-sim_bar_chart.xaxis.axis_label = 'question'
-
-# --- interactions -----------------------------------------------------------
-
-def selected_search_result_handler(attr, old, new):
-    Data.selected_index = new['1d']['indices'][0]
-    Data.update_sims_source(Data.selected_index)
-
-    tr_code = Data.search_res_df.iloc[Data.selected_index].name
-    sim_bar_chart.title.text = 'Top {} similar questions for question {}'.format(MAX_BARS, tr_code)
-
-Data.search_res_source.on_change('selected', selected_search_result_handler)
-
-qtext = TextInput(title="Search question text")
-qtext.on_change('value', lambda attr, old, new: Data.update_search_res_source(qtext.value))
-
-# --- final layout -----------------------------------------------------------
-
-sizing_mode = 'fixed'
-
-desc = Div(text=open(join(dirname(__file__), "description.html")).read(), width=PAGE_WIDTH)
-
-inputs = widgetbox(*[qtext], sizing_mode=sizing_mode)
-
-l = layout([
-    [desc],
-    [inputs],
-    [search_res_table],
-    [sim_bar_chart]
-], sizing_mode=sizing_mode)
-
-curdoc().add_root(l)
-curdoc().title = "Question bank exploration dashboard"
-
-show(l)
+App.init()
